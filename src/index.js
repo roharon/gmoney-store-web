049595154d7f23480ebb91444506b2bd49cea441
roharon <roharon98@gmail.com>
2020-05-09 22:35:01 +0900

    Fix: viewport height size hidden by url bar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
