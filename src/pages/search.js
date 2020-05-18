550a7ab6f1ab728f1138c7187c871961b469d4ad
roharon <roharon98@gmail.com>
2020-05-19 01:02:13 +0900

    Feat: search store

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,34 +1,51 @@
-import React, { useState } from 'react';
-import { WhiteSpace } from 'antd-mobile';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {SearchBar} from '../component/search';
 import StoreCard from '../component/storeCard';
 import './page.css';
 
 
 const Search = () => {
-  const [address, setAddress] = useState(localStorage.getItem('sigoon'));
-  const [search, setSearch] = useState();
-  const [emptyMsg, setEmptyMsg] = useState("가맹점이 없습니다")
+  const [search, setSearch] = useState("");
+  const [store, setStore] = useState({});
+  const [emptyShow, setEmptyShow] = useState(false);
+  const [page, setPage] = useState(0);
+  const sigoon = localStorage.getItem('sigoon');
+  const emptyMsg = "찾으시는 가맹점이 없습니다"
 
-  const handleOnChange = (e) => {
-    localStorage.setItem('sigoon', e.target.value);
-    const sigoon = localStorage.getItem('sigoon');
-    setAddress(sigoon);
-  };
+  useEffect(() => {
+    const getStore = async () => {
+      await axios
+        .get('/api/v1/store/search', {
+          params: {
+            title: search,
+            sigoon: sigoon,
+            page: page
+          }
+        })
+        .then(data => {
+          setStore(data.data.data.content)
+        })
+    }
 
-  //TODO: Infinite-scroll 적용 or pagniation-bar
+    if (search.length >= 3){
+      getStore()
+      setEmptyShow(search ? true : false)
+    }
+  }, [search, page, sigoon])
 
+  //TODO: infinite scroll 추가
   return (
     <div className="content">
       <SearchBar
-        placeholder={"가맹점 검색하기"}
+        placeholder={"가맹점 검색 (3글자 이상)"}
         onChange={(e) => setSearch(e.target.value)}
       />
       <StoreCard
-        Item={{}}
+        Item={store}
+        emptyShow={emptyShow}
         emptyMsg={emptyMsg}
       />
-
     </div>
   )
 };
